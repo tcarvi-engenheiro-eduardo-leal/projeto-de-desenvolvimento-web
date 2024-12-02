56ae92c519afa1dfa2f70efdcd04934ff41d07f6
tcarvi <tcarvi@me.com>
2024-12-02 15:29:45 -0300

    Update

--- nao_enviar/adonisjs-projects/2/starwars-api/ace.js
+++
@@ -1,6 +0,0 @@
-import 'reflect-metadata'
-import { Ignitor } from '@adonisjs/core/build/standalone.js'
-
-new Ignitor(__dirname)
-  .httpServer()
-  .start()
--- nao_enviar/adonisjs-projects/4/starwars-api/app/Controllers/Http/PersonController.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-
-class PersonController {
-}
-
-module.exports = PersonController
